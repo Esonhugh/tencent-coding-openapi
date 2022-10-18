2676211b7733e5b273a7e26c05df4f752f4df96c
Esonhugh <esonhughoutside@gmail.com>
2022-10-18 14:34:57 +0800

    feat(define): add define of Deopt

--- OpenApi/baseClient/define.go
+++ OpenApi/baseClient/define.go
@@ -1,5 +1,28 @@
 package baseClient
 
+// ProjectObject define
+/*
+`
+	{
+	"Name": "coding-demo",
+	"Id": 2,
+	"Type": 2,
+	"DisplayName": "示例项目",
+	"Icon": "https://codehub.cn/7971.png",
+	"Description": "CODING 示例项目",
+	"CreatedAt": 1572933083682,
+	"MaxMember": 0,
+	"TeamId": 12,
+	"UserOwnerId": 0,
+	"IsDemo": true,
+	"Archived": false,
+	"StartDate": 0,
+	"UpdatedAt": 1572933083682,
+	"TeamOwnerId": 0,
+	"EndDate": 0,
+	"Status": 1
+}`
+*/
 type ProjectObject struct {
 	Id          int    `json:"Id"`
 	CreatedAt   int64  `json:"CreatedAt"`
@@ -18,4 +41,28 @@ type ProjectObject struct {
 	TeamId      int    `json:"TeamId"`
 	IsDemo      bool   `json:"IsDemo"`
 	Archived    bool   `json:"Archived"`
+}
+
+// DeoptObject define.
+/*
+`
+	{
+	"Id": 8521598,
+	"Name": "demo",
+	"HttpsUrl": "https://e.coding.net/codingcorp/pual_test/demo.git",
+	"ProjectId": 8104562,
+	"SshUrl": "git@e.coding.net:codingcorp/pual_test/demo.git",
+	"WebUrl": "https://codingcorp.coding.net/p/pual_test/d/demo",
+	"VcsType": "git"
+	}
+`
+*/
+type DeoptObject struct {
+	Id        int    `json:"Id"`
+	Name      string `json:"Name"`
+	HttpsUrl  string `json:"HttpsUrl"`
+	ProjectId int    `json:"ProjectId"`
+	SshUrl    string `json:"SshUrl"`
+	WebUrl    string `json:"WebUrl"`
+	VscType   string `json:"VscType"`
 }
