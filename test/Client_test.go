b807c8b2150a8cd109aeb16a223cac1a1fb0ecec
Esonhugh <esonhughoutside@gmail.com>
2022-10-19 19:47:01 +0800

    update(filter json): update

--- test/Client_test.go
+++ test/Client_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"github.com/esonhugh/tencent-coding-openapi/OpenApi"
-	"github.com/esonhugh/tencent-coding-openapi/OpenApi/sdk/project"
 	"github.com/esonhugh/tencent-coding-openapi/OpenApi/sdk/team"
 	"testing"
 )
@@ -13,17 +12,6 @@ func Test_Client(t *testing.T) {
 		PageSize:    10,
 		PageNumber:  1,
 		ProjectName: "",
-	})
-	if err != nil {
-		t.Error(err)
-	}
-	t.Log(meInfo)
-}
-
-func Test_Client2(t *testing.T) {
-	c := OpenApi.NewClient()
-	meInfo, err := c.DescribeProjectByName(project.DescribeProjectByNameReq{
-		ProjectName: "srun4-portal-atour",
 	})
 	if err != nil {
 		t.Error(err)
